5cba01e1e901d1dd1cd117ad2f027f1fa1edf83b
mismyl123 <mlalonde309@gmail.com>
2021-03-12 21:07:21 -0500

    final roman solution

--- roman.js
+++ roman.js
@@ -9,9 +9,16 @@
 // D             500
 // M             1000
 
-// For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+// For example, 2 is written as II in Roman numeral, 
+//just two one's added together. 12 is written as XII, which is simply X + II. 
+//The number 27 is written as XXVII, which is XX + V + II.
 
-// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+// Roman numerals are usually written largest to smallest from left to right.
+// However, the numeral for four is not IIII. 
+//Instead, the number four is written as IV. 
+//Because the one is before the five we subtract it making four. 
+//The same principle applies to the number nine, which is written as IX. 
+//There are six instances where subtraction is used:
 
 //     I can be placed before V (5) and X (10) to make 4 and 9. 
 //     X can be placed before L (50) and C (100) to make 40 and 90. 
@@ -61,13 +68,13 @@
  * @return {number}
  */
     const roman = {
-        I = 1,
-        V = 5,
-        X = 10,
-        L = 50,
-        C = 100,
-        D = 500,
-        M = 1000
+        I : 1,
+        V : 5,
+        X : 10,
+        L : 50,
+        C : 100,
+        D : 500,
+        M : 1000
     };
 
     var romanToInt = function(s) {
@@ -100,3 +107,31 @@
 // LLLL=CC,
 // DDDD=MM,
 // DDDDD=MMD,
+
+const romanToInt = (s) => {
+    const vals = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    let result = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        let val = s[i];
+        // Corresponding integer for the current char
+        let curr = vals[val];
+        result += curr;
+    
+        if ((val === "V" || val === "X") && s[i - 1] === "I") {
+        // subtract -2: 1 for already counted 'I', 1 from 'X' or 'V'
+        result -= 2;
+        }
+        if ((val === "L" || val === "C") && s[i - 1] === "X") {
+        // --
+        result -= 20;
+        }
+        if ((val === "D" || val === "M") && s[i - 1] === "C") {
+        // --
+        result -= 200;
+        }
+    }  
+    return result;
+    
+    };
+    
