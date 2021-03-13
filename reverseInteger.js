854f982be741ccc6379843852bf3d28ed4644b20
mismyl123 <mlalonde309@gmail.com>
2021-03-12 19:56:33 -0500

    first attempt roman

--- reverseInteger.js
+++ reverseInteger.js
@@ -1,7 +1,10 @@
 
-// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+// Given a signed 32-bit integer x, return x with its digits reversed. 
+//If reversing x causes the value to go outside the signed 32-bit integer range
+// [-2^31, 2^31 - 1], then return 0.
 
-// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+// Assume the environment does not allow you to store 64-bit integers 
+//(signed or unsigned).
 
  
 
@@ -37,11 +40,23 @@
  * @return {number}
  */
 var reverse = function(x) {
-   let number = Math.abs(x).toString.split('').reverse().join('');
-    //     if( x > -2^31 || x < 2^31); {
-    //      return 0;
-    
-    // else{
-    return number;
-    
+    let number = Math.abs(x).toString().split('').reverse().join('');
+    if(number > 2147483647) {return 0}
+    return(x < 0? -1: 1) * number
+};
+
+var reverse = function(x) {
+    let positive = true;
+    if (x < 0) {
+        positive = false;
+        x *= -1;
+    }
+    let reversedNumber = parseInt(x.toString().split('').reverse().join(''));
+    if (!positive) {
+        reversedNumber *= -1;
+    }
+    if (reversedNumber > 2147483647 || reversedNumber < -2147483648) {
+        return 0;
+    }
+	return reversedNumber;
 };
